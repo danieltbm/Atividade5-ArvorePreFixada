7bc67488932e42695b037938005be6c4df9da185
danieltbm <163066884+danieltbm@users.noreply.github.com>
2024-04-08 22:16:07 -0300

    Add files via upload

---
+++ ProjetoPreFixado/src/modelagem/Numero.java
@@ -0,0 +1,18 @@
+package modelagem;
+
+public class Numero {
+	private int dado;
+
+	public int getDado() {
+		return dado;
+	}
+
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+
+	@Override
+	public String toString() {
+		return "Dado: " + getDado();
+	}
+}
